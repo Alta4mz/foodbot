b19ceaa0dcc89d092de6246c9bf996cdb80022ba
Alta4mz <kukockiy@gmail.com>
2022-05-15 15:13:36 +0300

    m

--- handlers/menu.py
+++ handlers/menu.py
@@ -19,3 +19,22 @@ def createTable():
         if sqlite_connection:
             sqlite_connection.close()
             print("Соединение закрыто.")
+def addFood(id, food_type, food_name, food_composition):
+    try:
+        sqlite_connection = sqlite3.connect("sqlite_menu.db")
+        cursor = sqlite_connection.cursor()
+        sqlite_insert_query = '''INSERT INTO food
+                                 (id, food_type, food_name, food_composition)
+                                 VALUES
+                                 (?, ?, ?, ?)'''
+        data_tuple = (id, food_type, food_name, food_composition)
+        cursor.execute(sqlite_insert_query, data_tuple)
+        sqlite_connection.commit()
+        print("Запись добавлена в таблицу. ")
+        cursor.close()
+    except sqlite3.Error as error:
+        print("При работе с SQLite возникла ошибка: ", error)
+    finally:
+        if sqlite_connection:
+            sqlite_connection.close()
+            print("Соединение с SQLite закрыто. ")
